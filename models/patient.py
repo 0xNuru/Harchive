af995b8bd53883229a63cd8546725f709b3203dc
Cyberguru1 <hamzacypher34@gmail.com>
2023-02-20 02:55:33 -0600

    update: models and engine

--- models/patient.py
+++ models/patient.py
@@ -1,22 +1,22 @@
-# #!/usr/bin/python3
-# """
-# patient module
-# """
-# from sqlalchemy import Column, ForeignKey, Integer, String, Date, Enum
-# from models.user import User
-# from sqlalchemy.orm import relationship
-# import sys
-# sys.path.insert(0, '..')
+#!/usr/bin/python3
+"""
+patient module
+"""
+from sqlalchemy import Column, ForeignKey, Integer, String, Date, Enum
+from models.user import User
+from sqlalchemy.orm import relationship
+import sys
+sys.path.insert(0, '..')
 
 
-# class Patient(User):
-#     """
-#         patient details
-#     """
-#     __tablename__ = "patients"
-#     dob = Column(Date, nullable=False)
-#     gender = Column(Enum, nullable=False)
-#     address = Column(String, nullable=False)
-#     # insuranceID = Column(String, nullable=False)
+class Patient(User):
+    """
+        patient details
+    """
+    __tablename__ = "patients"
+    dob = Column(Date, nullable=False)
+    gender = Column(Enum, nullable=False)
+    address = Column(String, nullable=False)
+    insuranceID = Column(String, nullable=False)
 
-#     # patient_record = relationship("records", back_populates="patients")
+    patient_record = relationship("records", backref="patients")
