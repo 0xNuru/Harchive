903f448380ba896cafe1e139e0652691a4f1f684
Nurudeen Ahmed <nurudeenmyeslf@gmail.com>
2023-02-24 23:55:17 +0100

    resolved alembic

--- models/patient.py
+++ models/patient.py
@@ -9,6 +9,7 @@ from models.user import Users
 from models.record import Record
 from sqlalchemy.orm import relationship
 
+
 class genderEnum(enum.Enum):
     M = "M"
     F = "F"
@@ -19,9 +20,9 @@ class Patient(Users):
         insurance details
     """
     __tablename__ = "patient"
-    id = Column(String, ForeignKey('user.id',  ondelete="CASCADE"), primary_key=True)
-    insuranceID = Column(String(128),nullable=True, unique=True)
+    id = Column(String, ForeignKey(
+        'user.id',  ondelete="CASCADE"), primary_key=True)
+    insuranceID = Column(String(128), nullable=True, unique=True)
     address = Column(String(128), nullable=False)
     role = Column(String(50), nullable=False, default='patient')
     patient_record = relationship(Record, cascade_backrefs='patient')
-    
