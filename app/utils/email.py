f47dbea5b4d5501f5ab2bb5f7c4032980e53027d
Cyberguru1 <hamzacypher34@gmail.com>
2023-11-25 11:23:17 -0600

    email spam fix

--- app/utils/email.py
+++ app/utils/email.py
@@ -50,10 +50,10 @@ class Email:
             MAIL_FROM=settings.EMAIL_FROM,
             MAIL_PORT=settings.EMAIL_PORT,
             MAIL_SERVER=settings.EMAIL_HOST,
-            MAIL_STARTTLS=False,
-            MAIL_SSL_TLS=True,
+            MAIL_STARTTLS=True,
+            MAIL_SSL_TLS=False,
             USE_CREDENTIALS=True,
-            VALIDATE_CERTS=True
+            VALIDATE_CERTS=False
         )
         #  Generate the HTML template
 
