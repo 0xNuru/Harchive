43b013a5c6698f58b31018ecf952e665c7740d07
Cyberguru1 <hamzacypher34@gmail.com>
2024-01-23 12:19:47 -0600

    fixed verfiy issue and refresh link

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 from utils.logger import logger
 from utils.oauth1 import AuthJWT
 from utils import auth
@@ -19,6 +20,7 @@ from jinja2 import Environment, select_autoescape, PackageLoader
 from utils.auth import oauth, OAuthError
 from utils import acl, utime
 from utils.email import verifyEmail
+from utils.cache import json_cache
 
 sys.path.insert(0, '..')
 
@@ -50,6 +52,21 @@ def verify_token(token: str, db: Session = Depends(load)):
     result = verifyToken(token)
 
     if not result:
+
+        # to do remove user details if existing
+
+        cached_data = json_cache.get(token)
+
+        if cached_data :
+            user = db.query_eng(userModel.Users).filter(
+                userModel.Users.email == cached_data.lower()).first()
+            db.delete(user)
+            db.save()
+            
+            # remove user cached details
+            json_cache.delete(token)
+
+
         raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE,
                             detail=[{"message":"Token for Email Verification has expired."}])
     else:
@@ -64,6 +81,9 @@ def verify_token(token: str, db: Session = Depends(load)):
         user_model.is_verified = True
         db.update(user_model)
         db.save()
+
+        # remove user cached details
+        json_cache.delete(token)
         
     return  {
         "status": "success",
@@ -141,6 +161,141 @@ def reset_password(request: userSchema.resetPassword, token: str, db: Session = 
         "message": "Password reset successfully"
     }
 
+# login endpoint
+@router.post('/login', status_code=status.HTTP_200_OK)
+async def login(response: Response, request: userSchema.UserLogin = Depends(),
+          Authorize: AuthJWT = Depends(), db: Session = Depends(load)):
+    duration = 3 # current timeout duration is 3 minutes
+    tryalls = 3 # current maximum retry is 3 times
+    email = request.email.lower()
+    password = request.password._secret_value
+
+    check = db.query_eng(userModel.Users).filter(
+        userModel.Users.email == email).first()
+
+    if not check:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+                            detail=[{"msg":f"Incorrect Username or Password"}])
+    
+    # check if user is currently suspended 
+    # current timeout value: 3 minutes
+    
+    if check.is_suspended and not utime.compare_time(check.suspended_at, duration):
+        # get the remaining time left
+        minute, secs = utime.getRemain_time(check.suspended_at, duration)
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail=[{"msg":f"Maximum login limit reached, try again in {minute} min {secs} secs"}])
+
+    # check if user is currently suspended 
+    # And compare the duration of suspension
+
+    if check.is_suspended and utime.compare_time(check.suspended_at, duration):
+        acl.reset_user_state(email) # reset user state if timeout is reached
+
+    patient = db.query_eng(patientModel.Patient).filter(
+        patientModel.Patient.id == check.id).first()
+
+    if not auth.verify_password(password, check.password_hash):
+        # initiate jail login 
+        acl.update_max_trys(email, tryalls)
+        remain_tryalls = tryalls - check.failed_login_attempts
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail=[{'msg':f'Incorrect Username or Password, {remain_tryalls} trys remainning'}])
+
+    # reset max login retry after succesfull login
+    if  check.failed_login_attempts is not None or check.failed_login_attempts > 0 :
+        acl.reset_user_state(email)
+    
+    if not check.is_verified:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail=[{'msg':'User not verified, please verify your email to continue'}])
+
+    data = {
+        'username': check.name,
+        'email': check.email,
+        'user_id': check.id,
+        'role': check.role
+    }
+
+    if patient:
+        data['nin'] = patient.nin
+    else:
+        data['nin'] = None
+
+    # generate user cookies
+    access_token = auth.access_token(data)
+    refresh_token = auth.refresh_token(data)
+
+    # # save tokens in the cookies
+    auth.set_access_cookies(access_token, response)
+    auth.set_refresh_cookies(refresh_token, response)
+
+    
+    return {
+        "status": "success",
+        "message": "user logged in successfully",
+        "tokens": {
+            "access_token": access_token,
+            "refresh_token": refresh_token
+            }
+        }
+
+@router.get('/refresh')
+async def refresh(request: Request,
+                  response: Response,
+                  Authorize: AuthJWT = Depends(),
+                  db: Session = Depends(load)):
+
+    try:
+
+        Authorize.jwt_refresh_token_required()
+
+        user_email = Authorize.get_jwt_subject()
+
+        if not user_email:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                detail=[{'msg':'Could not refresh access token'}])
+
+        check = db.query_eng(userModel.Users).filter(
+            userModel.Users.email == user_email).first()
+
+        if not check:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                detail=[{'msg':'The user belonging to this token no logger exist'}])
+    except Exception as e:
+        error = e.__class__.__name__
+        if error == 'MissingTokenError':
+            redirect_url = request.url_for('login')
+            
+            return JSONResponse(content={
+                "redirect_url": redirect_url,
+                "redirect": True
+                }, status_code=307)
+        
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail=error)
+
+    data = {
+        'username': check.name,
+        'email': check.email,
+        'user_id': check.id
+
+    }
+
+    # generate user cookies
+    access_token = auth.access_token(data)
+
+    # save tokens in the cookies
+    auth.set_access_cookies(access_token, response)
+
+    return {
+            "status": "success",
+            "message": "refreshed successfully!!",
+            "tokens": {
+                "access_token": access_token,
+                }
+            }
+
 # login with google endpoint
 @router.route('/auth/login_with_google')
 async def login(request: Request):
@@ -213,153 +368,14 @@ async def auth_google_login(request: Request,
     # save tokens in the cookies
     logger.info(f" {check.name} logged in !!")
 
-    return JSONResponse({
+    return {
         "status": "success",
         "message": "user logged in successfully",
         "tokens": {
             "access_token": access_token,
             "refresh_token": refresh_token
         }
-        })
-    
-
-# login endpoint
-@router.post('/login', status_code=status.HTTP_200_OK)
-async def login(response: Response, request: userSchema.UserLogin = Depends(),
-          Authorize: AuthJWT = Depends(), db: Session = Depends(load)):
-    duration = 3 # current timeout duration is 3 minutes
-    tryalls = 3 # current maximum retry is 3 times
-    email = request.email.lower()
-    password = request.password._secret_value
-
-    check = db.query_eng(userModel.Users).filter(
-        userModel.Users.email == email).first()
-
-    if not check:
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT,
-                            detail=[{"msg":f"Incorrect Username or Password"}])
-    
-    # check if user is currently suspended 
-    # current timeout value: 3 minutes
-    
-    if check.is_suspended and not utime.compare_time(check.suspended_at, duration):
-        # get the remaining time left
-        minute, secs = utime.getRemain_time(check.suspended_at, duration)
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                            detail=[{"msg":f"Maximum login limit reached, try again in {minute} min {secs} secs"}])
-
-    # check if user is currently suspended 
-    # And compare the duration of suspension
-
-    if check.is_suspended and utime.compare_time(check.suspended_at, duration):
-        acl.reset_user_state(email) # reset user state if timeout is reached
-
-    patient = db.query_eng(patientModel.Patient).filter(
-        patientModel.Patient.id == check.id).first()
-
-    if not auth.verify_password(password, check.password_hash):
-        # initiate jail login 
-        acl.update_max_trys(email, tryalls)
-        remain_tryalls = tryalls - check.failed_login_attempts
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
-                            detail=[{'msg':f'Incorrect Username or Password, {remain_tryalls} trys remainning'}])
-
-    # reset max login retry after succesfull login
-    if  check.failed_login_attempts is not None or check.failed_login_attempts > 0 :
-        acl.reset_user_state(email)
-    
-    if not check.is_verified:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                            detail=[{'msg':'User not verified, please verify your email to continue'}])
-
-    data = {
-        'username': check.name,
-        'email': check.email,
-        'user_id': check.id,
-        'role': check.role
-    }
-
-    if patient:
-        data['nin'] = patient.nin
-    else:
-        data['nin'] = None
-
-    # generate user cookies
-    access_token = auth.access_token(data)
-    refresh_token = auth.refresh_token(data)
-
-    # # save tokens in the cookies
-    # auth.set_access_cookies(access_token, response)
-    # auth.set_refresh_cookies(refresh_token, response)
-    # logger.info(f" {check.name} logged in !!")
-
-    # return "Login successfull !!"
-    
-    
-    return JSONResponse({
-        "status": "success",
-        "message": "user logged in successfully",
-        "tokens": {
-            "access_token": access_token,
-            "refresh_token": refresh_token
-            }
-        })
-
-@router.get('/refresh')
-async def refresh(request: Request,
-                  response: Response,
-                  Authorize: AuthJWT = Depends(),
-                  db: Session = Depends(load)):
-
-    try:
-
-        Authorize.jwt_refresh_token_required()
-
-        user_email = Authorize.get_jwt_subject()
-
-        if not user_email:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                                detail=[{'msg':'Could not refresh access token'}])
-
-        check = db.query_eng(userModel.Users).filter(
-            userModel.Users.email == user_email).first()
-
-        if not check:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                                detail=[{'msg':'The user belonging to this token no logger exist'}])
-    except Exception as e:
-        error = e.__class__.__name__
-        if error == 'MissingTokenError':
-            redirect_url = request.url_for('login')
-            
-            return JSONResponse(content={
-                "redirect_url": redirect_url,
-                "redirect": True
-                }, status_code=307)
-        
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail=error)
-
-    data = {
-        'username': check.name,
-        'email': check.email,
-        'user_id': check.id
-
-    }
-
-    # generate user cookies
-    access_token = auth.access_token(data)
-
-    # save tokens in the cookies
-    auth.set_access_cookies(access_token, response)
-
-    return JSONResponse({
-            "status": "success",
-            "message": "refreshed successfully!!",
-            "tokens": {
-                "access_token": access_token,
-                }
-            })
+        }
 
 @router.get('/logout', status_code=status.HTTP_200_OK)
 def logout(response: Response, Authorize: AuthJWT = Depends()):
